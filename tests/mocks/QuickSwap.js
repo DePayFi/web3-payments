222c380b6d689bb4a3b5f9e34a59e95de02086b4
Sebastian Pape <spape@users.noreply.github.com>
2022-05-21 20:30:17 +0200

    adds polygon support (#6)
    * github workflows node version 16
    
    * v10.3.0: adds polygon support
    
    * fix web3-exchange version on dev page

---
+++ tests/mocks/QuickSwap.js
@@ -0,0 +1,68 @@
+import { ethers } from 'ethers'
+import { findByName } from '@depay/web3-exchanges'
+import { mock } from '@depay/web3-mock'
+
+let exchange = findByName('quickswap')
+
+let mockPair = (provider, pair, params)=>{
+  mock({
+    provider: provider,
+    blockchain: 'polygon',
+    call: {
+      to: pair,
+      api: exchange.contracts.pair.api,
+      method: 'getReserves',
+      return: [ethers.utils.parseUnits('1000', 18), ethers.utils.parseUnits('1000', 18), '1629804922']
+    }
+  })
+  mock({
+    provider: provider,
+    blockchain: 'polygon',
+    call: {
+      to: pair,
+      api: exchange.contracts.pair.api,
+      method: 'token0',
+      return: params[0]
+    }
+  })
+  mock({
+    provider: provider,
+    blockchain: 'polygon',
+    call: {
+      to: pair,
+      api: exchange.contracts.pair.api,
+      method: 'token1',
+      return: params[1]
+    }
+  })
+  mock({
+    provider,
+    blockchain: 'polygon',
+    call: {
+      to: exchange.contracts.factory.address,
+      api: exchange.contracts.factory.api,
+      method: 'getPair',
+      params: params,
+      return: pair
+    }
+  })
+}
+
+let mockAmounts = ({ provider, method, params, amounts })=>{
+  return mock({
+    provider,
+    blockchain: 'polygon',
+    call: {
+      to: exchange.contracts.router.address,
+      api: exchange.contracts.router.api,
+      method,
+      params,
+      return: amounts
+    }
+  })
+}
+
+export {
+  mockPair,
+  mockAmounts,
+}
