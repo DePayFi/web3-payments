548d1e50cd87d6157d3725a5ddf780eb3f5397d3
Sebastian Pape <s@pape.io>
2022-08-07 19:17:32 +0200

    v10.6.1: depay.com

--- tests/mocks/api.js
+++ tests/mocks/api.js
@@ -2,7 +2,7 @@ import fetchMock from 'fetch-mock'
 
 let mockAssets = ({ blockchain, delay, account, assets })=>{
   fetchMock.get({
-      url: `https://public.depay.fi/accounts/${blockchain}/${account}/assets`,
+      url: `https://public.depay.com/accounts/${blockchain}/${account}/assets`,
       overwriteRoutes: true,
       delay
     }, assets
