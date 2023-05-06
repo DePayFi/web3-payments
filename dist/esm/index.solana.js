a03b0ec499048b97be3bc141f6db0508c4cbaf84
Sebastian Pape <s@pape.io>
2023-05-06 12:19:13 +0200

    update dependencies

--- dist/esm/index.solana.js
+++ dist/esm/index.solana.js
@@ -22637,7 +22637,7 @@ SystemProgram.programId = new PublicKey('11111111111111111111111111111111'); // 
 // TODO: replace 300 with a proper constant for the size of the other
 // Transaction fields
 
-var CHUNK_SIZE$1 = PACKET_DATA_SIZE - 300;
+var CHUNK_SIZE$2 = PACKET_DATA_SIZE - 300;
 /**
  * Program loader interface
  */
@@ -22866,7 +22866,7 @@ var Loader = /*#__PURE__*/function () {
   return Loader;
 }();
 
-Loader.chunkSize = CHUNK_SIZE$1;
+Loader.chunkSize = CHUNK_SIZE$2;
 var BPF_LOADER_PROGRAM_ID = new PublicKey('BPFLoader2111111111111111111111111111111111');
 /**
  * Factory class for transactions to interact with a program loader
@@ -40698,17 +40698,6 @@ var u8$1 = lib.u8;
 var vec = lib.vec;
 lib.vecU8;
 
-class StaticJsonRpcSequentialProvider$1 extends Connection {
-
-  constructor(url, network, endpoints, failover) {
-    super(url);
-    this._network = network;
-    this._endpoint = url;
-    this._endpoints = endpoints;
-    this._failover = failover;
-  }
-}
-
 let _window$1;
 
 let getWindow$1 = () => {
@@ -40720,6 +40709,106 @@ let getWindow$1 = () => {
   }
   return _window$1
 };
+
+const getConfiguration$1 = () =>{
+  if(getWindow$1()._Web3ClientConfiguration === undefined) {
+    getWindow$1()._Web3ClientConfiguration = {};
+  }
+  return getWindow$1()._Web3ClientConfiguration
+};
+
+const BATCH_INTERVAL$1 = 10;
+const CHUNK_SIZE$1 = 99;
+
+class StaticJsonRpcSequentialProvider$1 extends Connection {
+
+  constructor(url, network, endpoints, failover) {
+    super(url);
+    this._provider = new Connection(url);
+    this._network = network;
+    this._endpoint = url;
+    this._endpoints = endpoints;
+    this._failover = failover;
+    this._pendingBatch = [];
+    this._rpcRequest = this._rpcRequestReplacement.bind(this);
+  }
+
+  requestChunk(chunk) {
+
+    const batch = chunk.map((inflight) => inflight.request);
+
+    return this._provider._rpcBatchRequest(batch)
+      .then((result) => {
+        // For each result, feed it to the correct Promise, depending
+        // on whether it was a success or error
+        chunk.forEach((inflightRequest, index) => {
+          const payload = result[index];
+          if (payload.error) {
+            const error = new Error(payload.error.message);
+            error.code = payload.error.code;
+            error.data = payload.error.data;
+            inflightRequest.reject(error);
+          } else {
+            inflightRequest.resolve(payload);
+          }
+        });
+      }).catch((error) => {
+        if(error && [
+          'Failed to fetch', '504', '503', '502', '500', '429', '426', '422', '413', '409', '408', '406', '405', '404', '403', '402', '401', '400'
+        ].some((errorType)=>error.toString().match(errorType))) {
+          const index = this._endpoints.indexOf(this._endpoint)+1;
+          this._endpoint = index >= this._endpoints.length ? this._endpoints[0] : this._endpoints[index];
+          this._provider = new Connection(this._endpoint);
+          this.requestChunk(chunk);
+        } else {
+          chunk.forEach((inflightRequest) => {
+            inflightRequest.reject(error);
+          });
+        }
+      })
+  }
+    
+  _rpcRequestReplacement(methodName, args) {
+
+    const request = { methodName, args };
+
+    if (this._pendingBatch == null) {
+      this._pendingBatch = [];
+    }
+
+    const inflightRequest = { request, resolve: null, reject: null };
+
+    const promise = new Promise((resolve, reject) => {
+      inflightRequest.resolve = resolve;
+      inflightRequest.reject = reject;
+    });
+
+    this._pendingBatch.push(inflightRequest);
+
+    if (!this._pendingBatchAggregator) {
+      // Schedule batch for next event loop + short duration
+      this._pendingBatchAggregator = setTimeout(() => {
+        // Get the current batch and clear it, so new requests
+        // go into the next batch
+        const batch = this._pendingBatch;
+        this._pendingBatch = [];
+        this._pendingBatchAggregator = null;
+        // Prepare Chunks of CHUNK_SIZE
+        const chunks = [];
+        for (let i = 0; i < Math.ceil(batch.length / CHUNK_SIZE$1); i++) {
+          chunks[i] = batch.slice(i*CHUNK_SIZE$1, (i+1)*CHUNK_SIZE$1);
+        }
+        chunks.forEach((chunk)=>{
+          // Get the request as an array of requests
+          chunk.map((inflight) => inflight.request);
+          return this.requestChunk(chunk)
+        });
+      }, getConfiguration$1().batchInterval || BATCH_INTERVAL$1);
+    }
+
+    return promise
+  }
+}
 
 const getAllProviders$2 = ()=> {
   if(getWindow$1()._Web3ClientProviders == undefined) {
@@ -40740,7 +40829,13 @@ const setProvider$1$1 = (blockchain, provider)=> {
 const setProviderEndpoints$1$1 = async (blockchain, endpoints, detectFastest = true)=> {
   
   getAllProviders$2()[blockchain] = endpoints.map((endpoint, index)=>
-    new StaticJsonRpcSequentialProvider$1(endpoint, blockchain, endpoints)
+    new StaticJsonRpcSequentialProvider$1(endpoint, blockchain, endpoints, ()=>{
+      if(getAllProviders$2()[blockchain].length === 1) {
+        setProviderEndpoints$1$1(blockchain, endpoints, detectFastest);
+      } else {
+        getAllProviders$2()[blockchain].splice(index, 1);
+      }
+    })
   );
 
   let provider;
@@ -40936,13 +41031,6 @@ const getProvider = async (blockchain)=>{
   } else {
     throw 'Unknown blockchain: ' + blockchain
   }
-};
-
-const getConfiguration$1 = () =>{
-  if(getWindow$1()._Web3ClientConfiguration === undefined) {
-    getWindow$1()._Web3ClientConfiguration = {};
-  }
-  return getWindow$1()._Web3ClientConfiguration
 };
 
 const accountInfo$1 = async ({ address, api, method, params, provider, block }) => {
